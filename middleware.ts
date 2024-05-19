ffd0463e2f4d7fd0fd0e95ffa07c5b370793790b
danielevans254 <danielevans254@yahoo.com>
2024-05-19 09:17:59 +0800

    Add configuration files and dependencies for Next.js project

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/",
+  // TODO: Add more protected routes here
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (isProtectedRoute(request)) {
+    auth().protect()
+  }
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
