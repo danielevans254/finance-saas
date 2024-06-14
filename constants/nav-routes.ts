9ae56c043d2c071afcdc3d7a413db4c4b8dadd35
danielevans254 <danielevans254@yahoo.com>
2024-06-14 10:50:26 +0800

    feat: Add navbar routes constants

---
+++ constants/nav-routes.ts
@@ -0,0 +1,31 @@
+type NavigationRoutes = {
+  label: string;
+  href: string;
+};
+
+export const navRoutes: NavigationRoutes[] = [
+  {
+    label: 'Home',
+    href: '/',
+  },
+  {
+    label: 'Transactions',
+    href: '/transactions',
+  },
+  {
+    label: 'Accounts',
+    href: '/accounts',
+  },
+  {
+    label: 'Categories',
+    href: '/categories',
+  },
+  {
+    label: 'Reports',
+    href: '/reports',
+  },
+  {
+    label: 'Settings',
+    href: '/settings',
+  },
+];
