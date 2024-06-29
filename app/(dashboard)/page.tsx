bd4f034d7836283d0d50747f7719345d68a3c39e
danielevans254 <danielevans254@yahoo.com>
2024-06-29 14:18:42 +0800

    refactor: Update dashboard page get accounts of the current user using the react query hook

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,34 @@
+'use client'
+
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import { accounts } from '../../db/schema/schema';
+
 export default function Home() {
+  const { data, error } = useGetAccounts();
   return (
-    <div>
-      Home Page
+    <div className="space-y-8 pt-8 px-4">
+      <ClerkLoaded>
+        {data && (
+          <ul className="bg-white shadow-md rounded-lg p-4 space-y-2">
+            {data.map((account) => (
+              <li key={account.id} className="flex items-center justify-between p-2 border-b last:border-b-0">
+                <span className="text-lg font-medium">{account.name}</span>
+                <span className="text-sm text-gray-500">#{account.id}</span>
+              </li>
+            ))}
+          </ul>
+        )}
+      </ClerkLoaded>
+      <ClerkLoading>
+        <div className="flex flex-col items-center justify-center space-y-2 pb-2">
+          <Loader2 className="w-8 h-8 text-black animate-spin" />
+          <p className="text-gray-700">Loading accounts</p>
+        </div>
+      </ClerkLoading>
+      {error && <p className="text-red-500 text-center">Error: {error.message}</p>
+      }
     </div>
   );
 }
