cb9dfbd613463651a0d08348809b5a261283d617
danielevans254 <danielevans254@yahoo.com>
2024-07-25 10:34:50 +0800

    refactor: Update accounts table schema
    - Add category and type columns to the accounts table
    - Add customAccountName and customTypeName columns to the accounts table
    - Remove unnecessary comment about updatedAt field

--- db/schema/schema.ts
+++ db/schema/schema.ts
@@ -1,11 +1,18 @@
 import { text, pgTable, timestamp } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
-  plaidId: text("plaid_id").notNull(),
+  plaidId: text("plaid_id"),
   name: text("name").notNull(),
+  category: text("category").notNull(),
+  type: text("type"),
+  customAccountName: text("custom_account_name"),
+  customTypeName: text("custom_type_name"),
   userId: text("user_id").notNull(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
-  // TODO: Ayo this snippet is from drizzle however it doesn't return the expected value, how tf can the updatedAt happen before the createdAt
   updatedAt: timestamp('updated_at', { mode: 'date', precision: 3 }).$onUpdate(() => new Date()),
+});
+
+export const insertAccountSchema = createInsertSchema(accounts, {
 });
