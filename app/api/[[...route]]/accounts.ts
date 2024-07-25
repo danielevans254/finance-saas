d37f9b7e7319bdc0b8856369afb94fd932fb52d5
danielevans254 <danielevans254@yahoo.com>
2024-07-25 20:37:14 +0800

    refactor: Update accounts API endpoint
    - Add authentication middleware to the GET and POST routes
    - Retrieve account data based on user ID
    - Return account data in the response
    - Add validation for creating a new account
    - Insert new account into the database with generated UUID

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,14 +1,48 @@
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
-import { accounts } from "@/db/schema/schema";
+import { accounts, insertAccountSchema } from "@/db/schema/schema";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth"
+import { eq } from 'drizzle-orm';
+import { zValidator } from "@hono/zod-validator"
+import { v4 as uuidv4 } from 'uuid';
+import { custom } from "zod";
 
-const account = new Hono().get("/", async (c) => {
-  const data = await db
-    .select({ id: accounts.id, name: accounts.name })
-    .from(accounts)
-  return c.json({
-    data
+const account = new Hono()
+  .get("/", clerkMiddleware(), async (c) => {
+    const auth = getAuth(c);
+
+    if (!auth?.userId) {
+      return c.json({
+        error: "Unauthorized"
+      }, 401);
+    }
+
+    const data = await db
+      .select({ id: accounts.id, name: accounts.name, category: accounts.category, type: accounts.type, customAccountName: accounts.customAccountName, customTypeName: accounts.customTypeName })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId))
+    return c.json({
+      data
+    });
+  })
+  .post("/", clerkMiddleware(), zValidator("json", insertAccountSchema.pick({ name: true, category: true, type: true })), async (c) => {
+    const auth = getAuth(c);
+    const values = c.req.valid("json")
+
+    if (!auth?.userId) {
+      return c.json({
+        error: "Unauthorized"
+      }, 401);
+    }
+
+    const [data] = await db.insert(accounts).values({
+      id: uuidv4(),
+      userId: auth.userId,
+      ...values
+    }).returning();
+    return c.json({
+      data
+    });
   });
-});
 
 export default account;
