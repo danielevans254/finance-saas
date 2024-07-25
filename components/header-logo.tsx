68f29d4d3ea4507013571d072186000b38219ba4
danielevans254 <danielevans254@yahoo.com>
2024-07-25 10:34:27 +0800

    refactor: Export HeaderLogo component as named export
    refactor: fix styles

--- components/header-logo.tsx
+++ components/header-logo.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const HeaderLogo = () => {
+export const HeaderLogo = () => {
   return (
     <Link href="/">
       <div className="items-center hidden lg:flex space-x-3 group">
@@ -12,12 +12,10 @@ const HeaderLogo = () => {
           height={100}
           className="transition-transform duration-300 group-hover:scale-110"
         />
-        <p className="font-semibold text-white text-2xl ml-2.5 transition-opacity duration-300 group-hover:opacity-80">
+        <p className="font-semibold text-white text-2xl ml-2.5 transition-opacity duration-300 group-hover:opacity-80 lg:hidden hidden">
           {process.env.NEXT_PUBLIC_APP_NAME}
         </p>
       </div>
     </Link>
   );
-}
-
-export default HeaderLogo;
+};
